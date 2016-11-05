304027c804782be054852a9984ac90b76776453a
SegFault42 <rabougue@student.42.fr>
2016-11-05 15:24:21 +0100

    ++ex13

---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rabougue <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/21 19:21:22 by rabougue          #+#    #+#             */
+/*   Updated: 2016/09/23 22:19:24 by rabougue         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb > 12 || nb < 0)
+		return (0);
+	else if (nb == 0)
+		return (1);
+	if (nb > 1 && nb < 13)
+		return (nb * ft_recursive_factorial(nb - 1));
+	return (nb);
+}
