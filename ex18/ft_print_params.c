045af5161325f565292ee622756d6d30023cd255
SegFault42 <rabougue@student.42.fr>
2016-11-05 16:19:26 +0100

    ++ex18

---
+++ ex18/ft_print_params.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rabougue <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/24 18:55:07 by rabougue          #+#    #+#             */
+/*   Updated: 2016/05/04 09:16:38 by rabougue         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		++i;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	if (argc > 1)
+	{
+		while (i < argc)
+		{
+			ft_putstr(argv[i]);
+			ft_putchar('\n');
+			++i;
+		}
+	}
+}
