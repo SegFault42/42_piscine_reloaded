d7f61636beb64b198b4cafb64b2a3043f86e9b54
SegFault42 <rabougue@student.42.fr>
2016-11-05 17:29:35 +0100

    ++ex19

---
+++ ex19/ft_sort_params.c
@@ -0,0 +1,77 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		++i;
+	return (i);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	if (ft_strlen(s1) > ft_strlen(s2))
+		return (1);
+	else if (ft_strlen(s1) < ft_strlen(s2))
+		return (-1);
+	while (s1[i] != '\0')
+	{
+		if (s2[i] != s1[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	char	*tmp;
+	int		find;
+	int		i;
+
+	find = 1;
+	while (find)
+	{
+		find = 0;
+		i = 0;
+		while (++i < argc - 1)
+		{
+			if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+			{
+				tmp = argv[i];
+				argv[i] = argv[i + 1];
+				argv[i + 1] = tmp;
+				find = 1;
+			}
+		}
+	}
+	i = 0;
+	while (++i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+	}
+	return (0);
+}
